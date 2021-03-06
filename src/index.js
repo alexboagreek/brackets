f96daca707a4a9e2a8781544d34775deef01eb0b
alexboagreek <olivesargreek@gmail.com>
2021-03-06 20:13:37 +0300

    modified: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  return '';
-}
+  for (i = str.length; i > 0; --i){
+      for (j = 0; j < bracketsConfig.length; j++){
+          let temp = bracketsConfig[j].join('');
+          let counter = str.includes(temp);
+          if (counter){
+              str =str.replace(temp, '');
+            }
+        }
+    } return str === "";
+} 
+
